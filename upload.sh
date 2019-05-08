669bd0c514c94df11f0ca1d7ff0423f7cb92f0bc
edoardoORANGE <49418326+edoardoORANGE@users.noreply.github.com>
2019-05-08 22:44:33 +0200

    Add files via upload

---
+++ upload.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git add .
+git commit -m "Add existing file"
+git push origin
+
+
+#-----nel caso non funzionasse vanno riportati i seguenti comandi...
+#git config --global user.email "dandrea.1694282@studenti.uniroma1.it"
+#git config --global user.name "edoardoORANGE"
+
+#-----verranno richiesti UserName e PassWord:
+#us. edoardoORANGE
+#ps. Mammaepapa1!
+
+#----per compilare ed eseguire----
+#chmod +rx upload.sh
+#./upload.sh
