bf788895ac93cee819880e24cc90de417dccf564
Orange02 <orange02@Orange02.phys.uniroma1.it>
2019-05-21 18:30:34 +0200

    Add existing file

--- Analisi/visualizzazioneEventi.py
+++ Analisi/visualizzazioneEventi.py
@@ -2,8 +2,8 @@ import ROOT as root
 import numpy as np
 import matplotlib.pyplot as plt
 
-fileNumber = 440
-fileROOT = root.TFile('../../Data/PMTgem/selfPMTgem2_' + str(fileNumber) + '.root')
+fileNumber = 450
+fileROOT = root.TFile('../../Data/PMTgem/CosmiciPMTgem2_' + str(fileNumber) + '.root')
 tree = fileROOT.Get("t1") #nome del datatree
 
 n = 1024
